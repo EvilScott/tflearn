b8862483ea91b136d045fa19aee660c09ab8faaa
EvilScott <reis.robert.s@gmail.com>
2016-12-09 17:40:29 -0800

    minor adjustments for an extra ~5% accuracy

--- mnist_basic.py
+++ mnist_basic.py
@@ -6,8 +6,9 @@ data, labels, test_data, test_labels = mnist.load_data(one_hot=True)
 
 # set up network
 input_layer = tfl.input_data(shape=[None, 784])
-hidden_layer = tfl.fully_connected(input_layer, 300)
-output_layer = tfl.fully_connected(hidden_layer, 10, activation='sigmoid')
+hidden_layer = tfl.fully_connected(input_layer, 300, activation='sigmoid')
+dropout = tfl.dropout(hidden_layer, 0.8)
+output_layer = tfl.fully_connected(dropout, 10, activation='sigmoid')
 regression = tfl.regression(output_layer, learning_rate=0.002)
 
 # create model
@@ -16,6 +17,6 @@ model = tfl.DNN(regression)
 # train model
 model.fit(data,
           labels,
-          n_epoch=2,
+          n_epoch=3,
           validation_set=(test_data, test_labels),
           show_metric=True)
